16381d700bda9a0ac233e256dc8913053fbe4cad
Trần Đình Thoại <codingore@gmail.com>
2016-09-08 23:05:09 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -11,7 +11,7 @@ foreach($_POST as $k=>$v) {
 $url .= 'botmethod=' . $method;
 
 if ($method == 'post') {
-    $url .= '&botdata=' . file_get_contents('php://input');
+    $url .= '&botdata=' . urlencode(file_get_contents('php://input'));
 }
 
 $data = file_get_contents($url);
