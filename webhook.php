9d18a70115a747e7237a0d7b26dfaec2d99d8505
Trần Đình Thoại <codingore@gmail.com>
2016-09-08 22:10:53 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -2,10 +2,10 @@
 
 $url = 'http://maikiem.sayobot.com/webhook.yo?';
 foreach($_GET as $k=>$v) {
-    $url .= str_replace('_', '.', $k) .'='.$v.'&';
+    $url .= str_replace('hub_', 'hub.', $k) .'='.$v.'&';
 }
 foreach($_POST as $k=>$v) {
-    $url .= str_replace('_', '.', $k) .'='.$v.'&';
+    $url .= str_replace('hub_', 'hub.', $k) .'='.$v.'&';
 }
 
 $data = file_get_contents($url);
